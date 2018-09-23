9c9da1872dde50e9e45c3aadf3322a7a942cd5c8
Mariia <just-another-log-in@yandex.ru>
2018-09-23 10:21:12 +0300

    fix vlidation

--- src/main/webapp/js/validation.js
+++ src/main/webapp/js/validation.js
@@ -2,15 +2,15 @@ function isFormFilled() {
     var y = document.getElementById('coordinate_y').value;
     var x = document.getElementById('coordinate_x').value;
     var radius = document.getElementById('radius').value;
-    if (y == "" || y < -3 || y > 3) {
+    if (y == "" || y <= -3 || y >= 3) {
         alert("Некорректное значение координаты У");
         return false;
     }
-    if (x == "" || x < -3 || x > 5) {
+    if (x == "" || x <= -3 || x >= 5) {
         alert("Некорректное значение координаты X");
         return false;
     }
-    if (radius == "" || radius < 2 || radius > 5) {
+    if (radius == "" || radius <= 2 || radius >= 5) {
         alert("Некорректное значение переменной R");
         return false;
     }
